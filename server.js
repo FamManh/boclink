ee48a24efe2de10fdff0395217120c9284d82021
manhphm0997 <manh.pham@digicommercegroup.com>
2020-12-23 14:56:37 +0700

    boclink

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+var morgan = require("morgan");
+const isbot = require("isbot");
+var path = require("path");
+var _ = require("lodash");
+var accessLogStream = fs.createWriteStream(__dirname + "/myLogFile.log", {
+  flags: "a",
+});
+// setup the logger
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.get("/", function (req, res) {
+  if (
+    isbot(req.get("user-agent")) ||
+    _.includes(req.get("user-agent"), "Chrome-Lighthouse")
+  ) {
+    res.sendFile(path.resolve('real/index.html'));
+  } else {
+    res.sendFile(path.resolve('fake/index.html'));
+  }
+});
+app.set("port", process.env.PORT || 8000);
+app.set("host", process.env.HOST || "localhost");
+
+app.listen(app.get("port"), function () {
+  console.log(
+    "%s server listening at http://%s:%s",
+    process.env.NODE_ENV,
+    app.get("host"),
+    app.get("port")
+  );
+});
