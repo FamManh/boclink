dfe055e02c11aee255b211f53c5ec85d6d77f58f
manhphm0997 <manh.pham@digicommercegroup.com>
2020-12-25 15:22:15 +0700

    add logger

--- server.js
+++ server.js
@@ -2,12 +2,28 @@ var express = require("express");
 var app = express();
 var fs = require("fs");
 var morgan = require("morgan");
+var winston = require("winston");
 const isbot = require("isbot");
 var path = require("path");
 var _ = require("lodash");
 var accessLogStream = fs.createWriteStream(__dirname + "/myLogFile.log", {
   flags: "a",
 });
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
 
 // setup the logger
 app.use(morgan("combined", { stream: accessLogStream }));
@@ -18,8 +34,16 @@ app.get("/", function (req, res) {
     isbot(req.get("user-agent")) ||
     _.includes(req.get("user-agent"), "Chrome-Lighthouse")
   ) {
+    logger.log({
+      level: 'info',
+      message: `BOT - ${req.get("user-agent")}`
+    });
     res.sendFile(path.resolve('real/index.html'));
   } else {
+    logger.log({
+      level: 'info',
+      message: `PER - ${req.get("user-agent")}`
+    });
     res.sendFile(path.resolve('fake/index.html'));
   }
 });
